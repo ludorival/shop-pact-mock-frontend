7accf7fa3b84da0367063bcd2dd5d4fa0ee93b41
ludorival <ludorival@gmail.com>
2025-11-09 21:50:55 +0100

    Revert "Step -2 - Publish Consumer Contract"

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from "cypress";
-import pactPlugin from 'pact-js-mock/lib/cypress/plugin'
 
 export default defineConfig({
   component: {
     devServer: {
       framework: "react",
       bundler: "vite",
-    },
-    setupNodeEvents(on, config) {
-      return pactPlugin(on, config)
     },
   },
 });
